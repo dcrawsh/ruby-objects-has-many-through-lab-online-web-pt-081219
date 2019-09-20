deec591722816148c4fcab53af09003656cb45a5
Dylan Crawshaw <dcrawsh@gmail.com>
2019-09-20 16:30:15 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -12,6 +12,13 @@ def.self.all
 end 
 
 def new_appointment(patient,date)
+appointment = Appointment.new(date,patient,self)
+end 
 
-  
+def appointments 
+Appointment.all.select{|apps| apps.doctor == self} 
+end 
+
+def patients
+appointments.map{|apps| apps.patient}
 end 
