aadd79df3771682d04c1813ef1d32014e9fd2acb
Dylan Crawshaw <dcrawsh@gmail.com>
2019-09-20 16:12:14 +0000

    Automatically backed up by Learn

---
+++ lib/genre.rb
@@ -0,0 +1,21 @@
+class Genre 
+attr_accessor :name 
+@@all = Array.new 
+
+def self.all
+@@all 
+end 
+
+def initialize(name)
+@name = name 
+end
+
+def songs 
+Song.all.select {|song| song.genre == self}
+end 
+
+def artists
+songs.map {|song| song.artist}  
+end 
+
+end 
